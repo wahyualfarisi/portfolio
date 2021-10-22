b77020c09cd3f7af608a8762fd177b384c1d9c95
wahyu alfarisi <wahyualfarisi@gmail.com>
2021-10-22 19:27:42 +0700

    header component

--- components/project/Project.js
+++ components/project/Project.js
@@ -16,9 +16,7 @@ const Project = () => {
                             <h3 className={styles.portfolio_item_description_title}>Point Of Sales</h3>
                             <p className={styles.portfolio_item_description_type}>Personal Project</p>
                             <p className={styles.portfolio_item_description_date}>2 Nov 2020</p>
-
                             <p className={styles.portfolio_item_description_info}>This project was created using react js, redux (state management) and sass for styling. This application displays a list of menus that are divided by category. You can also order directly by adding to the order, and confirm by entering your personal data and also your order data can be seen in the transaction list</p>
-
                             <h3>Build With : </h3>
                             <ul className={styles.portfolio_item_description_stack}>
                                 <li>React JS</li>
@@ -37,8 +35,8 @@ const Project = () => {
                         </figure>
                     </div>
                     <div className={styles.portfolio_item_action}>
-                        <button>Preview</button>
-                        <button>Source Code</button>
+                        <button className={styles.btn}>Preview</button>
+                        <button className={styles.btn}>Source Code</button>
                     </div>
                 </li>
             </ul>
