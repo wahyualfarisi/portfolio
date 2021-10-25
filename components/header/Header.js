3588b4487cf517eb6585017e51c97c62aa29fd89
wahyu alfarisi <wahyualfarisi@gmail.com>
2021-10-25 20:30:29 +0700

    add activeColorMenu

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,18 +1,30 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Logo from '../ui/logo/Logo';
 import styles from './Header.module.scss';
 
 const Header = () => {
+  const params = useRouter();
+
+  console.log( params );
+
+
     return (
         <header className={styles.header}>
             <Link href="/"><a><Logo /></a></Link> 
             <nav className={styles.header_menu}>
               <ul className={styles.header_menu_lists}>
                 <li className={styles.header_menu_lists_item}>
-                  <Link href="/"><a>Home</a></Link>
+                  <Link 
+                    href="/">
+                    <a className={params.pathname === '/' ? "activeLink" : '' } >Home</a>
+                  </Link>
                 </li>
                 <li className={styles.header_menu_lists_item}>
-                  <Link href="/project"><a>Project</a></Link>
+                  <Link 
+                    href="/project">
+                      <a className={params.pathname === '/project' ? "activeLink" : ''}>Project</a>
+                  </Link>
                 </li>
               </ul>
             </nav>
